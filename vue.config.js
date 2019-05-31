7c830bf62727a08ae503e8aace317fc87a7d28e8
miracle <amyunfei@163.com>
2019-05-31 17:27:24 +0800

    11

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     // 基本路径
-    publicPath: process.env.NODE_ENV === "production" ? "/player-pages" : "./",
+    publicPath: "/player-pages",
     // 输出文件目录
     outputDir: 'dist',
     // eslint-loader 是否在保存的时候检查
